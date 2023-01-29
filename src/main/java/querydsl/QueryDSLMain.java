d6e7e12437ac7f0dbbb252d8f95cf615ac9ab6d7
kadun1 <kadunjj@gmail.com>
2023-01-29 23:50:16 +0900

    querydsl join

--- src/main/java/querydsl/QueryDSLMain.java
+++ src/main/java/querydsl/QueryDSLMain.java
@@ -1,11 +1,7 @@
 package querydsl;
 
-import com.mysema.query.QueryModifiers;
-import com.mysema.query.SearchResults;
 import com.mysema.query.jpa.impl.JPAQuery;
-import querydsl.domain.Item;
-import querydsl.domain.Member;
-import querydsl.domain.QItem;
+import querydsl.domain.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -27,7 +23,7 @@ public class QueryDSLMain {
         member1.setName("회원1");
         member1.setAge(10);
         em.persist(member1);
-        tx.commit();
+
 
         JPAQuery query = new JPAQuery(em);
         //QMember qMember = new QMember("m"); //생성되는 JPQL의 별칭이 m (직접 지정)
@@ -47,24 +43,53 @@ public class QueryDSLMain {
 
         QItem item = QItem.item;
 
-        SearchResults<Item> result = query.from(item)
-                .where(item.price.gt(20000))
-                .orderBy(item.price.desc(), item.stockQuantity.asc())
-                .offset(10).limit(20)
-                .listResults(item);
+//        SearchResults<Item> result = query.from(item)
+//                .where(item.price.gt(20000))
+//                .orderBy(item.price.desc(), item.stockQuantity.asc())
+//                .offset(10).limit(20)
+//                .listResults(item);
+//
+//        long total = result.getTotal();
+//        long limit = result.getLimit();
+//        long offset = result.getOffset();
+//        List<Item> results = result.getResults();
 
-        long total = result.getTotal();
-        long limit = result.getLimit();
-        long offset = result.getOffset();
-        List<Item> results = result.getResults();
-
-        System.out.println(total +" "+ limit +" "+ offset);
-
+        //이렇게도 페이징 사용 가능
 //        QueryModifiers queryModifiers = new QueryModifiers(20L, 10L);
 //        List<Item> list = query.from(item)
 //                .restrict(queryModifiers)
 //                .list(item);
 
+        //그룹함수 사용
+        query.from(item)
+                .groupBy(item.price)
+                .having(item.price.gt(1000))
+                .list(item);
 
+        QOrder order = QOrder.order;
+        QOrderItem orderItem = QOrderItem.orderItem;
+
+        query.from(order)
+                .join(order.member, member)
+                .leftJoin(order.orderItems, orderItem)
+                .list(order);
+
+        //join 조건을 거는 on절
+        query.from(order)
+                .leftJoin(order.orderItems, orderItem)
+                .on(orderItem.count.gt(2))
+                .list(order);
+
+        //fetch join
+        query.from(order)
+                .innerJoin(order.member, member).fetch()
+                .leftJoin(order.orderItems, orderItem).fetch()
+                .list(order);
+
+        query.from(order, member)
+                .where(order.member.eq(member))
+                .list(order);
+
+        tx.commit();
     }
 }
---
+++ src/main/java/querydsl/domain/Order.java
@@ -0,0 +1,23 @@
+package querydsl.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+}
